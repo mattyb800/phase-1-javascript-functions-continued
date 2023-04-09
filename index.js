b85ec24aa2e32d7ea7e483936fd9613d8e6a7a79
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-09 11:41:59 -0400

    functions cont lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+function wrapAdjective(flair = "*") {
+    return function(compliment = 'special') {
+        return `You are ${flair}${compliment}${flair}!`;
+}
+}
